d1b484851385dc2aab9a14c7c6a1059f638efbd2
zurdozarazaga <zurdozarazaga@gmail.com>
2022-05-02 18:29:31 -0300

    SEO with Helmet

--- weather-service/src/components/LayoutHelmet.jsx
+++ weather-service/src/components/LayoutHelmet.jsx
@@ -1,31 +1,14 @@
-import React from 'react'
-import { Helmet } from 'react-helmet'
+import React from "react";
+import { Helmet } from "react-helmet";
 
 const LayoutHelmet = ({ title, description }) => {
   return (
     <Helmet>
-      {title && <title>{title}</title>}
-      {description && <meta name="description" content= {description} />}
-      <link rel="shortcut icon" href="https://firebasestorage.googleapis.com/v0/b/portfolio-347820.appspot.com/o/logo%2Ffavicon.ico?alt=media&token=82813f21-8d71-4d11-bee4-e4b7ba5d6453" />
-      {/* <link rel="canonical" href="https://zurdozarazaga.github.io/weather-app/" />
-      <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
-      <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
-      <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
-      <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
-      <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
-      <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
-      <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
-      <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
-      <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-      <link rel="manifest" href="/manifest.json" />
-      <meta name="msapplication-TileColor" content="#ffffff" />
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-      <meta name="theme-color" content="#ffffff" /> */}
+      {title && <title>{`Weather Service | ${title}`}</title>}
+      {description && <meta name="description" content={description} />}
+      <link rel="icon" href="logo.svg" />
     </Helmet>
-  )};
+  );
+};
 
 export default LayoutHelmet;
--- weather-service/src/components/Report.jsx
+++ weather-service/src/components/Report.jsx
@@ -7,57 +7,69 @@ import LayoutHelmet from "./LayoutHelmet";
 import Spinner from "./Spinner";
 
 const Report = () => {
-  
   const [stateweather, setStateWeather] = useState({});
   console.log(stateweather);
-  
+
   const weatherData = useWeatherDate();
   console.log(weatherData);
-  const { country, name, day, description, temp, fahrenheit, image, humidity, wind, loading, error } = weatherData;
-  
- 
-    if (weatherData) {
-      
-    }
-    return(
-      <>
-      <LayoutHelmet title={'Weather Service'} description={'Aplicación para buscar los datos del tiempo en tu ciudad'} />
+  const {
+    country,
+    name,
+    day,
+    description,
+    temp,
+    fahrenheit,
+    image,
+    humidity,
+    wind,
+    loading,
+    error,
+  } = weatherData;
+
+  if (weatherData) {
+  }
+  return (
+    <>
+      <LayoutHelmet
+        title={`${name} ${temp ? temp : ""} °C`}
+        description={"Aplicación para buscar los datos del tiempo en tu ciudad"}
+      />
       {error && <div className="text-center text-red-600">{error}</div>}
       {loading && <Spinner />}
       {!loading && !error && (
-      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 bg-white pl-4 pr-4">
-        <div className=" md:col-span-2 flex justify-center items-center h-8 border-b-2 border-black">
-          Reporte del Tiempo
+        <div className="grid gap-4 grid-cols-1 md:grid-cols-2 bg-white pl-4 pr-4">
+          <div className=" md:col-span-2 flex justify-center items-center h-8 border-b-2 border-black">
+            Reporte del Tiempo
+          </div>
+          <div className="col-span-1 block">
+            <div>
+              <h3 className="text-gray-400">{country}</h3>
+            </div>
+            <div>
+              <h1 className="text-gray-400 text-2xl mt-2">{name}</h1>
+            </div>
+            <div className="text-gray-900 text-3xl mt-2">{day}</div>
+            <div className="text-gray-900 text-2xl mt-2">{description}</div>
+            <div className="text-gray-900 text-5xl mt-2">{`${temp} °C`}</div>
+            <div className="text-gray-900 text-xl mt-2">{`${fahrenheit} °F`}</div>
+          </div>
+          <div className="col-span-1 block">
+            <div>
+              <img src={image} alt="icon clima"></img>
+            </div>
+            <div className="text-gray-900 text-base mt-2">
+              prob de precipitaciones: 0%
+            </div>
+            <div className="text-gray-900 text-base mt-2">
+              {`Humedad: ${humidity} %`}
+            </div>
+            <div className="text-gray-900 text-base mt-2">
+              {`Viento: ${wind} km/h`}
+            </div>
+          </div>
         </div>
-        <div className="col-span-1 block">
-          <div>
-            <h3 className="text-gray-400">{country}</h3>
-          </div>
-          <div>
-            <h1 className="text-gray-400 text-2xl mt-2">{name}</h1>
-          </div>
-          <div className="text-gray-900 text-3xl mt-2">{day}</div>
-          <div className="text-gray-900 text-2xl mt-2">{description}</div>
-          <div className="text-gray-900 text-5xl mt-2">{`${temp} °C`}</div>
-          <div className="text-gray-900 text-xl mt-2">{`${fahrenheit} °F`}</div>
-        </div>
-        <div className="col-span-1 block">
-          <div>
-            <img src={image} alt="icon clima"></img>
-          </div>
-          <div className="text-gray-900 text-base mt-2">
-            prob de precipitaciones: 0%
-          </div>
-          <div className="text-gray-900 text-base mt-2">
-            {`Humedad: ${humidity} %`}
-          </div>
-          <div className="text-gray-900 text-base mt-2">
-            {`Viento: ${wind} km/h`}
-          </div>
-        </div>
-      </div>
       )}
-     </> 
+    </>
   );
 };
 
